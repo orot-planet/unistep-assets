e6822c8af7fffbd9679f7cacb0d0b4702d671a7d
orotplanet <barrierfreeorot@gmail.com>
2026-05-12 09:29:04 +0900

    Use system Chromium via PUPPETEER_EXECUTABLE_PATH

--- scripts/html-to-png.js
+++ scripts/html-to-png.js
@@ -50,6 +50,7 @@ async function convertAll() {
 
   const browser = await puppeteer.launch({
     headless: 'new',
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
     args: [
       '--no-sandbox',
       '--disable-setuid-sandbox',
